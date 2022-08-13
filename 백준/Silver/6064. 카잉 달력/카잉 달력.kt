30ffc262cc4f1882b22ed997d8d4d36dffd3a479
Wonseok Kim <76798309+onseok@users.noreply.github.com>
2022-08-13 19:39:38 +0900

    [Silver I] Title: 카잉 달력, Time: 376 ms, Memory: 23576 KB -BaekjoonHub

---
+++ 백준/Silver/6064. 카잉 달력/카잉 달력.kt
@@ -0,0 +1,18 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val t = br.readLine().toInt()
+    for (i in 0 until t) {
+        var ans = -1
+        val (m, n, x, y) = br.readLine().split(" ").map { it.toInt() }
+        for (i in x .. (n*m) step m) {
+            if ((i - y) % n == 0) {
+                ans = i
+                break
+            }
+        }
+        println(ans)
+    }
+}
