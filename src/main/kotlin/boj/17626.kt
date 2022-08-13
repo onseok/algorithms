9f8e990a82cc8ff5c9da3e63c5923527804dd3c4
wonseok <dnjstjr245@gmail.com>
2022-08-13 16:59:27 +0900

    :sparkles: [DP] 백준/실3/17626 (Four Squares)

---
+++ src/main/kotlin/boj/17626.kt
@@ -0,0 +1,28 @@
+package boj
+
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import kotlin.math.min
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val n = br.readLine().toInt()
+
+    val dp = IntArray(n + 1)
+    dp[1] = 1
+    var min = 0
+    for (i in 2..n) {
+        min = Int.MAX_VALUE
+
+        // i에서 i보다 작은 제곱수에서 뺀 dp[i - j * j] 중
+        // 최소를 택한다.
+        var j = 1
+        while (j * j <= i) {
+            val temp = i - j * j
+            min = min(min, dp[temp])
+            j++
+        }
+        dp[i] = min + 1 // 그리고 1을 더해준다.
+    }
+    print(dp[n])
+}
