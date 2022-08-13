d0344332286163bdfbf9e069cd5b8665b5b42707
wonseok <dnjstjr245@gmail.com>
2022-08-14 00:53:03 +0900

    :sparkle: [문자열] 백준/실1/5525 (IOIOI)

---
+++ src/main/kotlin/boj/5525.kt
@@ -0,0 +1,28 @@
+package boj
+
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val n = br.readLine().toInt()
+    val m = br.readLine().toInt()
+    val s = br.readLine().toCharArray()
+    var ans = 0
+    var cnt = 0
+    var i = 1
+    while (i < m-1) {
+        if(s[i-1] == 'I' && s[i] == 'O' && s[i+1] == 'I') {
+            cnt++
+            if (cnt == n) {
+                cnt--
+                ans++
+            }
+            i += 2
+        } else {
+            cnt = 0
+            i += 1
+        }
+    }
+    println(ans)
+}
