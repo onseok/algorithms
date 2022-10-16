a3d120919a90e2f51f39c7e35477a2aaae0a0d0d
Wonseok Kim <76798309+onseok@users.noreply.github.com>
2022-10-16 21:33:29 +0900

    [Gold III] Title: 줄 세우기, Time: 632 ms, Memory: 73472 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.kt
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.kt
@@ -1,8 +1,15 @@
 import java.io.BufferedReader
+import java.io.BufferedWriter
 import java.io.InputStreamReader
+import java.io.OutputStreamWriter
 import java.util.*
 
+/**
+ * @author onseok
+ */
+
 fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
     val (n, m) = readLine().split(" ").map { it.toInt() }
     val adj = ArrayList<ArrayList<Int>>()
     for (i in 0..n + 1) {
@@ -20,9 +27,10 @@ fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
     for (i in 1..n) {
         if (deg[i] == 0) q.offer(i)
     }
+
     while (!q.isEmpty()) {
         val cur = q.poll()
-        print("$cur ")
+        bw.write("$cur ")
         val list: List<Int> = adj[cur]
         for (i in list.indices) {
             val nxt = list[i]
@@ -30,4 +38,9 @@ fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
             if (deg[nxt] == 0) q.offer(nxt)
         }
     }
+
+    close()
+    bw.flush()
+    bw.close()
+
 }
