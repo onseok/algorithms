79c32ad30ff681a6c5c0c30962091a96343b71c1
Wonseok Kim <76798309+onseok@users.noreply.github.com>
2022-10-21 22:38:23 +0900

    [Gold V] Title: 다각형의 면적, Time: 240 ms, Memory: 27872 KB -BaekjoonHub

---
+++ 백준/Gold/2166. 다각형의 면적/다각형의 면적.kt
@@ -0,0 +1,29 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import kotlin.math.abs
+
+fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
+    val n = readLine().toInt()
+
+    val x = LongArray(n + 1)
+    val y = LongArray(n + 1)
+
+    var sumA = 0L
+    var sumB = 0L
+
+    repeat(n) { idx ->
+        val (a, b) = readLine().split(" ").map { it.toLong() }
+        x[idx] = a
+        y[idx] = b
+    }
+
+    x[n] = x[0]
+    y[n] = y[0]
+
+    for (i in 0 until n) {
+        sumA += x[i] * y[i + 1]
+        sumB += x[i + 1] * y[i]
+    }
+
+    println(String.format("%.1f", abs(sumA - sumB) / 2.0))
+}
