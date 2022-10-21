5aa431de29628b3b76a24a1467ddc1843cc253eb
Wonseok Kim <76798309+onseok@users.noreply.github.com>
2022-10-21 18:14:54 +0900

    [Gold II] Title: 공항, Time: 240 ms, Memory: 22960 KB -BaekjoonHub

---
+++ 백준/Gold/10775. 공항/공항.kt
@@ -0,0 +1,42 @@
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+
+lateinit var par: IntArray
+fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+    val gateNum = readLine().toInt()
+    val planeNum = readLine().toInt()
+    par = IntArray(gateNum + 1) { i -> i }
+
+    var ans = 0
+
+    for (i in 0 until planeNum) {
+        val g = readLine().toInt()
+        val emptyGate = find(g)
+        if (emptyGate == 0) {
+            break
+        }
+        ans++
+        union(emptyGate, emptyGate - 1)
+    }
+
+    bw.write("$ans")
+    bw.flush()
+    bw.close()
+    close()
+}
+
+private fun find(x: Int): Int {
+    if (x != par[x]) par[x] = find(par[x])
+    return par[x]
+}
+
+private fun union(a: Int, b: Int) {
+    val aParent = find(a)
+    val bParent = find(b)
+    if (aParent != bParent) {
+        par[aParent] = bParent
+    }
+}
