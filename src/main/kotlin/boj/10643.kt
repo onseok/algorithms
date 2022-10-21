49cd7c6c23bc2bd3de09a4227ce21673312691ec
wonseok <dnjstjr245@gmail.com>
2022-10-21 22:12:45 +0900

    :sparkles: [#1] 백준/브2/10643/FUNGHI

---
+++ src/main/kotlin/boj/10643.kt
@@ -0,0 +1,22 @@
+package boj
+
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import kotlin.math.max
+
+/**
+ * @author onseok
+ */
+
+fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
+    val arr = ArrayList<Int>()
+    var ans = Int.MIN_VALUE
+    for (i in 0 until 8) {
+        val s = readLine().toInt()
+        arr.add(s)
+    }
+    repeat(8) { idx ->
+        ans = max(ans, arr[(idx % 8)] + arr[(idx + 1) % 8] + arr[(idx + 2)  % 8] + arr[(idx + 3) % 8])
+    }
+    println(ans)
+}
