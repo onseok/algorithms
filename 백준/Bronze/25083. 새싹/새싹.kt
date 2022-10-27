0f9267c5e5bb860534740e32c7b2da677f25d0b5
Wonseok Kim <76798309+onseok@users.noreply.github.com>
2022-10-28 02:03:13 +0900

    [Bronze V] Title: 새싹, Time: 80 ms, Memory: 11672 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.kt
@@ -0,0 +1,13 @@
+import java.lang.StringBuilder
+
+fun main() {
+    val sb = StringBuilder()
+    sb
+        .append("         ,r'\"7\n")
+        .append("r`-_   ,'  ,/\n")
+        .append(" \\. \". L_r'\n")
+        .append("   `~\\/\n")
+        .append("      |\n")
+        .append("      |")
+    print(sb.toString())
+}
