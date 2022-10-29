22708da6aa91df94a193cc05b644a8cd97ff47e8
Wonseok Kim <76798309+onseok@users.noreply.github.com>
2022-10-29 20:53:33 +0900

    [Gold V] Title: 감소하는 수, Time: 120 ms, Memory: 14236 KB -BaekjoonHub

---
+++ 백준/Gold/1038. 감소하는 수/감소하는 수.kt
@@ -0,0 +1,23 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
+    val arr = ArrayList<Long>()
+
+    for (i in 1 .. 1023) {
+        var num = 0L
+        var tmp = i
+        for (j in 9 downTo 0) {
+            if(tmp % 2 == 1) num = 10 * num + j
+            tmp /= 2
+        }
+        arr.add(num)
+    }
+
+    arr.sort()
+
+    val n = readln().toInt()
+
+    if (n > 1022) println("-1")
+    else println(arr[n])
+}
