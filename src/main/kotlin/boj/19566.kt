8cc60bdebe319a5043303ff988af1d0d70065f60
wonseok <dnjstjr245@gmail.com>
2022-10-30 22:55:41 +0900

    :art: [#2] 백준/골2/19566/수열의 구간 평균

--- src/main/kotlin/boj/19566.kt
+++ src/main/kotlin/boj/19566.kt
@@ -1,4 +1,23 @@
 package boj
 
-class `19566` {
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
+    val input = readln().split(" ")
+    val n = input[0].toInt()
+    val k = input[1].toLong()
+    val arr = readln().split(" ").map { it.toLong() }
+
+    val prefixSum = LongArray(n + 1) { i -> if (i == 0) 0 else arr[i - 1] }
+    var result = 0L
+    val map = mutableMapOf<Long, Long>()
+    for (i in 1 until n + 1) {
+        prefixSum[i] += prefixSum[i - 1]
+        val calc = prefixSum[i] - k * i
+
+        result += map.getOrDefault(calc, 0L)
+        map[calc] = map.getOrDefault(calc, 0L) + 1
+    }
+    println(result + map.getOrDefault(0, 0))
 }
