d7b90dce19364b5901244f3e5ada5133782c4897
Wonseok Kim <76798309+onseok@users.noreply.github.com>
2022-10-30 21:28:13 +0900

    [Silver II] Title: 제곱수의 합, Time: 136 ms, Memory: 13152 KB -BaekjoonHub

---
+++ 백준/Silver/1699. 제곱수의 합/제곱수의 합.kt
@@ -0,0 +1,17 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
+    val n = readln().toInt()
+    val dp = IntArray(100_001)
+    for (i in 1 until n + 1) {
+        dp[i] = i
+        for (j in 1 until i) {
+            if (j * j > i) break
+            if (dp[i] > dp[i - j * j] + 1) {
+                dp[i] = dp[i - j * j] + 1
+            }
+        }
+    }
+    println(dp[n])
+}
