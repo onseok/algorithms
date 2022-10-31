3e2890978f978960327f22dafd99316b35830fc8
Wonseok Kim <76798309+onseok@users.noreply.github.com>
2022-10-31 11:40:54 +0900

    [Gold V] Title: 최소비용 구하기, Time: 784 ms, Memory: 79048 KB -BaekjoonHub

---
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.kt
@@ -0,0 +1,54 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.util.PriorityQueue
+
+private lateinit var adj: ArrayList<ArrayList<Node>>
+private lateinit var dist: IntArray
+private lateinit var check: BooleanArray
+fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
+    val n = readln().toInt()
+    val m = readln().toInt()
+    adj = ArrayList() // 인접 리스트
+    for (i in 0..n) {
+        adj.add(ArrayList())
+    }
+    dist = IntArray(n + 1) { Int.MAX_VALUE } // 시작점에서 각 정점으로 가는 최단거리
+    check = BooleanArray(n + 1) // 방문 확인
+
+    for (i in 0 until m) {
+        val (start, end, weight) = readln().split(" ").map { it.toInt() }
+        adj[start].add(Node(end, weight))
+    }
+
+    val (startPos, endPos) = readln().split(" ").map { it.toInt() }
+    println(dijkstra(startPos, endPos))
+}
+
+private fun dijkstra(start: Int, end: Int): Int {
+    val pq: PriorityQueue<Node> = PriorityQueue()
+    pq.offer(Node(start, 0))
+    dist[start] = 0
+
+    while (pq.isNotEmpty()) {
+        val curNode = pq.poll()
+        val cur = curNode.end
+
+        if (!check[cur]) {
+            check[cur] = true
+
+            for (node in adj[cur]) {
+                if(!check[node.end] && dist[node.end] > dist[cur] + node.weight) {
+                    dist[node.end] = dist[cur] + node.weight
+                    pq.add(Node(node.end, dist[node.end]))
+                }
+            }
+        }
+    }
+    return dist[end]
+}
+
+data class Node(val end: Int, val weight: Int) : Comparable<Node> {
+    override fun compareTo(other: Node): Int {
+        return weight - other.weight
+    }
+}
