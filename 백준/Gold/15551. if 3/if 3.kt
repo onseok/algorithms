8c3cdd63a886b1cf845d38ba43c77702a4d8a90c
Wonseok Kim <76798309+onseok@users.noreply.github.com>
2022-11-04 15:33:28 +0900

    [Gold III] Title: if 3, Time: 96 ms, Memory: 12596 KB -BaekjoonHub

---
+++ 백준/Gold/15551. if 3/if 3.kt
@@ -0,0 +1,9 @@
+import java.lang.StringBuilder
+fun main() {
+    val n = readln().toInt()
+    val sb = StringBuilder()
+    for (i in 2 until n) {
+        sb.append("*")
+    }
+    System.out.printf("%s>>\n%s=]\n", sb.toString(), sb.toString())
+}
